137291fc85597bebfbebfc2f65d198c0e3bf695b
ajlopez <webmaster@ajlopez.com>
2011-08-22 19:12:12 -0300

    appclient, appserver in numbers example

--- examples/numbers/server2.js
+++
@@ -1,9 +0,0 @@
-
-net = require('net');
-
-var server = net.createServer(function (socket) {
-	console.log('New connection');
-	socket.write('Hi, client!', 'utf8');
-});
-
-server.listen(3000, 'localhost');
