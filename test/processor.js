56edb9b620754fc5ca454bcd86b657c91dfab46b
ajlopez <webmaster@ajlopez.com>
2013-04-27 18:42:05 -0300

    Processor redesign: instead of emit action name, emit 'message', with function processor at constructor

--- test/processor.js
+++ test/processor.js
@@ -21,38 +21,42 @@ exports['Create Simple Processor for Application'] = function(test) {
 };
 
 exports['Process Simple Action'] = function(test) {
-	var processor = ajfabriq.createProcessor({ application: 'webcrawler' });
+	var processor = ajfabriq.createProcessor(
+        { application: 'webcrawler' },
+        function (msg) {
+            test.ok(msg);
+            test.equal(msg.application, 'webcrawler');
+            test.equal(msg.action, 'harvest');
+            test.equal(msg.url, 'http://www.google.com');
+            test.done();
+        }
+    );
+    
     var message = { application: 'webcrawler', action: 'harvest', url: 'http://www.google.com' };
     
     test.expect(5);
     test.ok(processor.canProcess(message));
-
-    processor.on('harvest', function(msg) {
-        test.ok(msg);
-        test.equal(msg.application, 'webcrawler');
-        test.equal(msg.action, 'harvest');
-        test.equal(msg.url, 'http://www.google.com');
-        test.done();
-    });
     
     processor.process(message);
 };
 
 exports['Process Action in Child Process'] = function(test) {
-	var processor = ajfabriq.createProcessor({ application: 'webcrawler' });
+	var processor = ajfabriq.createProcessor(
+        { application: 'webcrawler' },
+        function(msg) {
+            test.ok(msg);
+            test.equal(msg.application, 'webcrawler');
+            test.equal(msg.action, 'harvest');
+            test.equal(msg.url, 'http://www.google.com');
+            test.done();
+        }
+    );
+    
     var child = processor.createProcessor({ node: 'harvester' });
     var message = { application: 'webcrawler', node: 'harvester', action: 'harvest', url: 'http://www.google.com' };
     
     test.expect(5);
     test.ok(processor.canProcess(message));
-
-    child.on('harvest', function(msg) {
-        test.ok(msg);
-        test.equal(msg.application, 'webcrawler');
-        test.equal(msg.action, 'harvest');
-        test.equal(msg.url, 'http://www.google.com');
-        test.done();
-    });
     
     processor.process(message);
 };
